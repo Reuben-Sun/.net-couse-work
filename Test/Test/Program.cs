b0d9a122ed56978cbebf55285646d2aae82a99e7
Reuben <2824016820@qq.com>
2021-09-13 15:26:02 +0800

    :bell: Test

---
+++ Test/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "   fly me   to   the moon  ";
+            string[] b = a.Split(" ");
+            foreach(var item in b)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
