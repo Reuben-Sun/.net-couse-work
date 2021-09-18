6db13fbe897e25e10d40444e75e4b5d0f099b7d4
Reuben <2824016820@qq.com>
2021-09-18 22:03:45 +0800

    :sparkles: course 3 finish

--- FileFormatting/FileFormatting/Form1.Designer.cs
+++ FileFormatting/FileFormatting/Form1.Designer.cs
@@ -28,13 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileNameText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lineNumText = new System.Windows.Forms.Label();
+            this.worldNumText = new System.Windows.Forms.Label();
+            this.summaryButton = new System.Windows.Forms.Button();
+            this.formatButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(434, 80);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件处理器";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(26, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileNameText
+            // 
+            this.fileNameText.AutoSize = true;
+            this.fileNameText.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.fileNameText.Location = new System.Drawing.Point(234, 124);
+            this.fileNameText.Name = "fileNameText";
+            this.fileNameText.Size = new System.Drawing.Size(97, 40);
+            this.fileNameText.TabIndex = 2;
+            this.fileNameText.Text = "Null";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(19, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "文件名：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(19, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 40);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "行数：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(19, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 40);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "字数：";
+            // 
+            // lineNumText
+            // 
+            this.lineNumText.AutoSize = true;
+            this.lineNumText.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lineNumText.Location = new System.Drawing.Point(162, 276);
+            this.lineNumText.Name = "lineNumText";
+            this.lineNumText.Size = new System.Drawing.Size(37, 40);
+            this.lineNumText.TabIndex = 6;
+            this.lineNumText.Text = "0";
+            // 
+            // worldNumText
+            // 
+            this.worldNumText.AutoSize = true;
+            this.worldNumText.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.worldNumText.Location = new System.Drawing.Point(162, 332);
+            this.worldNumText.Name = "worldNumText";
+            this.worldNumText.Size = new System.Drawing.Size(37, 40);
+            this.worldNumText.TabIndex = 7;
+            this.worldNumText.Text = "0";
+            // 
+            // summaryButton
+            // 
+            this.summaryButton.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.summaryButton.Location = new System.Drawing.Point(227, 188);
+            this.summaryButton.Name = "summaryButton";
+            this.summaryButton.Size = new System.Drawing.Size(166, 59);
+            this.summaryButton.TabIndex = 8;
+            this.summaryButton.Text = "统计数据";
+            this.summaryButton.UseVisualStyleBackColor = true;
+            this.summaryButton.Click += new System.EventHandler(this.summaryButton_Click);
+            // 
+            // formatButton
+            // 
+            this.formatButton.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.formatButton.Location = new System.Drawing.Point(434, 188);
+            this.formatButton.Name = "formatButton";
+            this.formatButton.Size = new System.Drawing.Size(166, 59);
+            this.formatButton.TabIndex = 9;
+            this.formatButton.Text = "格式化";
+            this.formatButton.UseVisualStyleBackColor = true;
+            this.formatButton.Click += new System.EventHandler(this.formatButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.formatButton);
+            this.Controls.Add(this.summaryButton);
+            this.Controls.Add(this.worldNumText);
+            this.Controls.Add(this.lineNumText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileNameText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label fileNameText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lineNumText;
+        private System.Windows.Forms.Label worldNumText;
+        private System.Windows.Forms.Button summaryButton;
+        private System.Windows.Forms.Button formatButton;
     }
 }
 
--- FileFormatting/FileFormatting/Form1.cs
+++ FileFormatting/FileFormatting/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,130 @@ namespace FileFormatting
 {
     public partial class Form1 : Form
     {
+        string filePath;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "*.cs|*.cs";
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileNameText.Text = file.SafeFileName;
+                filePath = file.FileName;
+                summaryFile();
+            }
+
+
+        }
+
+        private void summaryButton_Click(object sender, EventArgs e)
+        {
+            summaryFile();
+        }
+
+        private void formatButton_Click(object sender, EventArgs e)
+        {
+            formatFile();
+            summaryFile();
+        }
+
+        void summaryFile()  //统计文件信息
+        {
+            if (filePath == null)
+            {
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 32; i <= 126; i++)
+            {
+                if (i >= 48 && i <= 57)  //数字
+                {
+                    continue;
+                }
+                if (i >= 65 && i <= 90)  //大写字母
+                {
+                    continue;
+                }
+                if (i >= 97 && i <= 122)
+                {
+                    continue;
+                }
+                sb.Append((char)i);
+            }
+            char[] splitList = sb.ToString().ToCharArray();
+
+            using (FileStream fs = File.OpenRead(filePath))
+            {
+
+                StreamReader sr = new StreamReader(fs); //读取行数
+                int lines = 0;
+                var ls = "";
+                int world = 0;
+                while ((ls = sr.ReadLine()) != null)
+                {
+                    lines++;
+                    string[] worldList = ls.Split(splitList);
+                    foreach (var item in worldList)
+                    {
+                        if (!String.IsNullOrEmpty(item))
+                        {
+                            world++;
+                        }
+                    }
+
+                }
+                lineNumText.Text = lines.ToString();
+                worldNumText.Text = world.ToString();
+            }
+
+
+        }
+
+
+        Regex mutiNote = new Regex("/\\*.*?\\*/", RegexOptions.Singleline);
+        Regex emptyLine = new Regex("^\\s*$[\\r\\n]*", RegexOptions.Multiline);
+        void formatFile()
+        {
+            if (filePath == null)
+            {
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+
+            using var sr = new StreamReader(filePath);
+            var result = sr.ReadToEnd();
+            var withoutSingleNote = Regex.Replace(result, "//.*", "");
+            var withoutMultiNote = mutiNote.Replace(withoutSingleNote, "");
+            var withoutEmptyLine = emptyLine.Replace(withoutMultiNote, "");
+            sr.Close();
+            using var sw = new StreamWriter(filePath);
+            sw.Write(withoutEmptyLine);
+            //var ls = "";
+            //while ((ls = sr.ReadLine()) != null)
+            //{
+            //    if (!String.IsNullOrWhiteSpace(ls))  //清除空行
+            //    {
+            //        int i = ls.IndexOf("//");
+            //        if (i == -1)
+            //        {
+            //            sb.AppendLine(ls);
+
+            //        }
+            //        else if (i != 0)
+            //        {
+            //            sb.AppendLine(ls.Substring(0, i));
+            //        }
+
+
+            //    }
+            //}
+
+
+
         }
     }
 }
