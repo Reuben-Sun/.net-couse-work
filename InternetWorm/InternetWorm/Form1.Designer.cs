7a17effe1d11df6ae7702e09e849f07907b2eedb
Reuben <2824016820@qq.com>
2021-10-11 20:48:58 +0800

    :star:course 5 finish

---
+++ InternetWorm/InternetWorm/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace InternetWorm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchContent = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // searchContent
+            // 
+            this.searchContent.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.searchContent.Location = new System.Drawing.Point(152, 12);
+            this.searchContent.Name = "searchContent";
+            this.searchContent.Size = new System.Drawing.Size(566, 53);
+            this.searchContent.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.searchButton.Location = new System.Drawing.Point(12, 12);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(134, 55);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "搜索";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 73);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(706, 365);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "电话号码";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "网址";
+            this.columnHeader2.Width = 300;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "编号";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchContent);
+            this.Name = "Form1";
+            this.Text = "网络爬虫";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchContent;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ InternetWorm/InternetWorm/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InternetWorm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Hashtable phoneTable = Hashtable.Synchronized(new Hashtable());
+        Hashtable urlTable = Hashtable.Synchronized(new Hashtable());
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(searchContent.Text))
+            {
+                MessageBox.Show("搜索内容为空", "警告");
+                return;
+            }
+            listView1.Items.Clear();
+            phoneTable.Clear();
+            urlTable.Clear();
+            var t1 = Task.Factory.StartNew(() => searchTask());
+            
+        }
+
+        string phoneRegex = @"1[3456789]\d{9}"; //匹配电话号码
+        //string webRegex = @"http://www\.baidu\.com(/[\w- ./?%&=]*)?";   //匹配百度的搜索结果的网页
+        string webRegex = @"http://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?";   //匹配所有网页
+
+        void searchTask()
+        {
+            Console.WriteLine("开始搜索");
+            string searchURL = @"http://www.baidu.com/s?wd=" + searchContent.Text;
+            WebClient client = new WebClient();
+            client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36");
+            client.Encoding = Encoding.UTF8;
+            string pageHtml = client.DownloadString(searchURL);
+
+
+            
+           
+            MatchCollection urlMatch = Regex.Matches(pageHtml, webRegex);
+            
+  
+            foreach (var _url in urlMatch)
+            {
+                if (phoneTable.Count >= 100)
+                {
+                    return;
+                }
+                if (urlTable.Contains(_url.ToString()))
+                {
+                    continue;
+                }
+                else
+                {
+                    urlTable.Add(_url.ToString(), _url.ToString());
+                    var t1 = Task.Factory.StartNew(() => getPhoneNumber(_url.ToString()));
+                }
+                
+                
+            }
+            
+        }
+
+        void getPhoneNumber(string url)
+        {
+           
+            try
+            {
+                WebClient client = new WebClient();
+                client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36");
+                client.Encoding = Encoding.UTF8;
+                string pageHtml2 = client.DownloadString(url);
+                MatchCollection mc = Regex.Matches(pageHtml2, phoneRegex);
+
+                //获得新的网页
+                MatchCollection umc = Regex.Matches(pageHtml2, webRegex);
+                foreach (var _url in umc)
+                {
+                    if(phoneTable.Count >= 100)
+                    {
+                        return;
+                    }
+                    if (urlTable.Contains(_url.ToString()))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        urlTable.Add(_url.ToString(), _url.ToString());
+                        var t1 = Task.Factory.StartNew(() => getPhoneNumber(_url.ToString()));
+                    }
+
+
+                }
+
+                this.Invoke((Action)(() =>
+                {
+
+                    foreach (var match in mc)
+                    {
+                        if (phoneTable.Contains(match.ToString()))
+                        {
+                            continue;
+                        }
+                        else if (phoneTable.Count >= 100)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            phoneTable.Add(match.ToString(), match.ToString());
+
+                            Console.WriteLine("找到一个电话号码");
+                            ListViewItem item = new ListViewItem();
+                            item.SubItems[0].Text = phoneTable.Count.ToString();
+                            item.SubItems.Add(match.ToString());
+                            item.SubItems.Add(url);
+                            listView1.Items.Add(item);
+                        }
+                        
+                        
+                    }
+                }));
+
+            }
+            catch (WebException)
+            {
+            }
+        }
+    }
+}
